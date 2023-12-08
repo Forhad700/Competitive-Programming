321fcd4878bba5b947b34de4bfc23f117947c43d
Forhad <71933188+Forhad700@users.noreply.github.com>
2023-12-08 22:30:12 +0600

    Delete Basic Programming Language directory

--- Basic Programming Language/Basic C++/const.cpp
+++
@@ -1,7 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
-      float const score=10.578;
-      cout<<score;
-}
--- Basic Programming Language/Basic C++/sum_series_range.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    int f,l,s;
-    cin>>f>>l;
-    s = l*(l+1)/2 - f*(f+1)/2+f;
-    cout<<s;
-}
